553502eb5af6c4e7a55848aa1148128a733f17cc
carlox-mass-ser <99211048+Carlox-ser-mass-ser@users.noreply.github.com>
2022-04-08 16:00:11 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -87,7 +87,7 @@ if(pattern.test(message.message)){
         if(Config.BGMFILTER){
         let banned = jid.find( Jid => Jid === message.jid);
         if(banned !== undefined) return
-        if (!!message.mention && message.mention[0] == '919895339960@s.whatsapp.net') {
+        if (!!message.mention && message.mention[0] == '91917356835535@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./files/file/p3r3.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted : message.data, ptt: true})
         }
         if (!!message.mention && message.mention[0] == Config.MENTION) {
